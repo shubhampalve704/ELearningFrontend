e9892088793de6e4a72b4cbf014dd313ad37ef7e
shubhampalve704 <shubhampalve704@gmail.com>
2024-08-27 23:52:28 +0530

    server updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-// export const server = "https://elearningserver2.onrender.com";
-export const server = "http://localhost:5000";
+export const server = "https://elearningserver2.onrender.com";
+// export const server = "http://localhost:5000";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
